81bfd067e8ab8278e18be3dd76784a527f34cd3d
RobiulHassan0 <robiulhassan270@gmail.com>
2025-09-16 05:58:27 +0600

    prject edited

--- login.js
+++
@@ -1,6 +0,0 @@
-
-function handleLogin(e) {
-    e.preventDefault();
-    alert("✅ সফলভাবে লগইন করা হয়েছে (ডেমো)।");
-    // এখানে ব্যাকেন্ড বা রিডাইরেক্ট নেই।
-}
--- register.js
+++
@@ -1,5 +0,0 @@
-
-function handleRegister(e) {
-    e.preventDefault();
-    alert("✅ রেজিস্ট্রেশন সম্পন্ন (ডেমো)।");
-}
